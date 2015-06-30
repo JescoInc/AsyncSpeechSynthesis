26745900f9f7f2652a595166befe3fc0ba452fb3
Jesse Glover <kohouri@gmail.com>
2015-06-29 20:36:15 -0400

    Async method for using Speech in Console App.

---
+++ AsyncSpeechSynth/AsyncSpeechSynth/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncSpeechSynth
+{
+    class Program
+    {
+        static void Main()
+        {
+            Example();
+            Console.ReadLine();
+        }
+
+        static async void Example()
+        {
+            string t = await Task.Run(() => runThis());
+        }
+
+        static string runThis()
+        {
+            string result = Console.ReadLine();
+
+            using (SpeechSynthesizer speech = new SpeechSynthesizer())
+            {
+                speech.SelectVoice("Microsoft Zira Desktop");
+                speech.Speak(result);
+            }
+            return result;
+        }
+    }
+}
